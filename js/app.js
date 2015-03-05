414a728c2955860abca4973d1af2ceaebe0c2039
Andrew Jongho Jung <andrewjjung47@gmail.com>
2015-03-04 22:21:06 -0500

    Made enemies respawn when they go over the right edge.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var intGenerator = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-var level = 4;
+var level = 10;
 
 // Enemies our player must avoid
 var Enemy = function() {
@@ -14,10 +14,7 @@ var Enemy = function() {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.width = 95; // 3px blank on each sides of the image
-    this.speed = Math.round(Math.random() * 350) + 50; // speed within range of [50, 400) px/s
-    this.x = Math.round(Math.random() * -level) * 100 -101; // prevent enemies to show up all at once
-    this.row = intGenerator(1, 3); // random position
-    this.y =  this.row * 83 - 23;
+    this.reset();
 };
 
 // Update the enemy's position, required method for game
@@ -26,6 +23,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x > 505) this.reset();
     this.x += this.speed * dt;
     this.left = this.x + 3; // furthest left pixel of the enemy bug
 };
@@ -33,6 +31,13 @@ Enemy.prototype.update = function(dt) {
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Enemy.prototype.reset = function() {
+    this.speed = Math.round(Math.random() * 350) + 50; // speed within range of [50, 400) px/s
+    this.x = Math.round(Math.random() * -level) * 100 -101; // prevent enemies to show up all at once
+    this.row = intGenerator(1, 3); // random position
+    this.y =  this.row * 83 - 23;
 };
 
 // Now write your own player class
